f578f717e2a59c3aa24950161bcbddf98a392a12
gonmendoza <gonmendoza1990@gmail.com>
2019-04-12 19:45:27 +0200

    fixed location of file

---
+++ src/sloth/__init__.py
@@ -0,0 +1,30 @@
+class dataframe:
+    def __init__(self, data, index=None, columns=None): #"dunder init" = double underscore init
+        self._data = data
+        if index is None:
+            for colname in data.keys():
+                last = len(data[colname])
+                for colname in data.keys():
+                    if len(data[colname]) != last:
+                        raise ValueError("FOOL! Your arrays must have the same size")
+                    else:
+                        self._index = np.array(range(len(data[colname])))
+        else:
+            self._index = index
+        if columns is None:
+            self._columns = self._data.keys()
+        else:
+            self._columns = columns
+    
+    @property
+    def data(self):
+        return self._data
+    @property
+    def index(self):
+        return self._index
+    @property
+    def columns(self):
+        return self._columns
+    def __repr__(self):
+        return f"[{self.data.items()}]"
+        
